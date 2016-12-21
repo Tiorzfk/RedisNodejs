3e31e9e2e8de097456a9b3175360f43939aaf985
tiorzfk <tiorezafk@yahoo.com>
2016-12-20 22:24:16 -0800

    get if not null, set and get if null

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,8 +4,35 @@ var redis = require('redis');
 var client = redis.createClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+router.get('/api/:key', function(req, res, next) {
+
+  client.on("error", function (err) {
+    console.log("Error " + err);
+  });
+
+  var key = req.params.key;
+
+  client.get(key, function(error, result) {
+
+      if (result) {
+        // the result exists in our cache - return it to our user immediately
+        res.send({ "key": result});
+      } else {
+
+        console.log('Simpan key baru pada redis.');
+
+        client.set(key,'Tio Ganteng',function(err,reply){
+          console.log('Berhasil menyimpan key baru');
+        });
+
+        client.get(key,function(err,data){
+          res.send({key:data});
+        });
+
+      }
+
+  });
+
 });
 
 module.exports = router;
